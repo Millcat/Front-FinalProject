32213ced5cec2285cbb1fc451e8d0667cfd36100
helene fav <helfav@outlook.fr>
2019-12-05 10:58:47 +0100

    components views css modified + packaje json

---
+++ src/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container-footer">
+        <h5>ABOUT US</h5>
+        <p>Conditions of use and sales</p>
+        <p>Cookies notice</p>
+      </div>
+      <div className="container-footer">
+        <h5>CONTACTS</h5>
+        <i class="fab fa-twitter-square">Twitter</i>
+        <i class="fab fa-facebook-square">Facebook</i>
+        <i class="fab fa-pinterest-square">Pinterest</i>
+        <i class="fab fa-instagram">Instagram</i>
+        <i class="fab fa-youtube">Youtube</i>
+      </div>
+      <div className="container-footer">
+        <h5>SITEMAP</h5>
+        <p>Blablablabla</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/NavMain.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+// import { NavLink } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const NavMain = () => {
+  return (
+    <Navbar expand="lg" className="nav">
+      <div className="container-nav">
+        <div className="container-nav-left">
+          <Navbar.Brand href="#home">
+            <img src="" id="logo-paris" alt="" />
+          </Navbar.Brand>
+          <Navbar id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">All experiences</Nav.Link>
+            </Nav>
+          </Navbar>
+        </div>
+        <div className="container-nav-right">
+          <NavDropdown
+            title="My experiences"
+            id="basic-nav-dropdown"
+            className="dropdown"
+          >
+            <NavDropdown.Item href="#action/3.1">
+              Create an experience
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Edit an experience
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#link">Sign In</Nav.Link>
+          <Nav.Link href="#link">Sign Up</Nav.Link>
+          <i class="fas fa-shopping-cart"></i>
+        </div>
+      </div>
+    </Navbar>
+  );
+};
+
+export default NavMain;
