484d2e97f103b9a5e404919d7ba2578cf27cf708
helene fav <helfav@outlook.fr>
2019-12-09 11:24:46 +0100

    app.js modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NotFound404 from "./views/NotFound404";
 
 // import the views
 import Home from "./views/Home";
@@ -13,11 +14,7 @@ import Footer from "./components/Footer";
 import CreateTour from "./views/CreateTour";
 import ManageTour from "./views/ManageTour";
 import EditTour from "./views/EditTour";
-<<<<<<< HEAD
 import Tour from "./views/Tour"
-=======
-import NotFound404 from "./views/NotFound404";
->>>>>>> 63ea4578fb8d822115b0a768842eb087595f1017
 
 function App() {
   return (
@@ -32,11 +29,8 @@ function App() {
         <Route path="/create-tour" component={CreateTour} />
         <Route path="/manage-tour" component={ManageTour} />
         <Route path="/edit-tour/:id" component={EditTour} />
-<<<<<<< HEAD
         <Route path="/tours/:id" component={Tour} />
-=======
         <Route path="/*" component={NotFound404} />
->>>>>>> 63ea4578fb8d822115b0a768842eb087595f1017
       </Switch>
       <Footer />
     </div>
